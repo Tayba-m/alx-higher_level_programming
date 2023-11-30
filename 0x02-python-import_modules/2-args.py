22270825570d5d9a1410fe3d62702ae95a15b1c3
Tayba-m~ <yanoomohamed@gmial.com>
2023-11-30 18:03:02 -0500

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     c = len(sys.argv) - 1
     if c == 0:
         print("0 arguments.")
     elif c == 1:
-        print("1 argumrnt:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(c))
     for t in range(c):
