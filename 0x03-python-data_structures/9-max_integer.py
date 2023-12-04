5315f1ab7c99d9080b4b67c27e9aa93b9bb68a72
Tayba-m~ <yanoomohamed@gmial.com>
2023-12-04 18:55:04 -0500

    commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return ("None")
     t = my_list[0]
-    for i in my_list:
+    for m in my_list:
         if m > t:
             t = m
     return (t)
